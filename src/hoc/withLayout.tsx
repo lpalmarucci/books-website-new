d578991f679c439e817e54fe12304001cd9f9611
lpalmarucci <lpalmarucci@gmail.com>
2022-12-05 20:26:27 +0100

    Changed component type in withLayout

--- src/hoc/withLayout.tsx
+++ src/hoc/withLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from "../components/Layout/Layout.component";
 
-const withLayout = (Component: any) => (props: any) => {
+const withLayout = (Component: React.FC<any>) => (props: any) => {
   return (
     <Layout>
       <Component {...props}/>
