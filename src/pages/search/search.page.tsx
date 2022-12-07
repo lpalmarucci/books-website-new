ae12fa8f93b735a68b513ffc978b2857ddcde28d
lpalmarucci <lpalmarucci@gmail.com>
2022-12-07 21:43:26 +0100

    Setup SearchPage

---
+++ src/pages/search/search.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import withLayout from "../../hoc/withLayout";
+
+const SearchPage: React.FC<any> = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default withLayout(SearchPage)
--- src/routes/common.routes.ts
+++ src/routes/common.routes.ts
@@ -2,13 +2,19 @@ import {lazy} from 'react';
 import {RouteType} from "./index";
 
 export enum RoutePaths{
-    HOME='/home'
+    HOME='/home',
+    SEARCH="/search",
+    SAVED="/saved"
 }
 
 export const commonRoutes: RouteType[] = [
     {
         component: lazy(() => import('../pages/home/home.page')),
         path: RoutePaths.HOME,
+    },
+    {
+        component: lazy(() => import('../pages/search/search.page')),
+        path: RoutePaths.SEARCH,
     },
     {
         component: lazy(() => import('../pages/404/404.page')),
